5fbeb861b01f7dfd90990d874b7c3585bfcd9d72
nekonyanneko <morita.d.shun@gmail.com>
2017-04-29 23:49:19 +0900

    不要なコードを削除

--- src/fasttext/load_ft.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import fasttext as ft
-
-model = ft.load_model('model.bin')
-print model.words # list of words in dictionary
-print model['king'] # get the vector of the word 'king'
